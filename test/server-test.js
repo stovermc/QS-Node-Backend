dfe60d3d90bcf3d09f9eb5ad16a582a568f8f5be
stovermc <stovermc@gmail.com>
2017-06-29 16:01:50 -0600

    Add test for all foods endpoint
    * set up directory for food model
    
    * set up database dependencies for food model

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,7 @@
 const assert = require("chai").assert
 const app = require("../server")
 const request = require("request")
+var pry = require('pryjs')
 
 describe('server', () => {
   before( (done) => {
@@ -13,15 +14,15 @@ describe('server', () => {
       baseUrl: 'http://localhost:9876/'
     })
   })
-  
+
   after( () => {
     this.server.close()
   })
-  
+
   it('should exist', function(){
     assert(app)
   })
-  
+
   describe('GET /', () => {
     it('should return a 200', (done) => {
       this.request.get('/', function(error, response) {
@@ -30,7 +31,7 @@ describe('server', () => {
         done()
       })
     })
-    
+
     it('should have a body with the name of the application', (done) => {
       this.request.get('/', function(error, response){
         if (error) { done(error) }
@@ -38,7 +39,32 @@ describe('server', () => {
         done()
       })
     })
-    
+
+    describe('GET /api/v1/foods', () => {
+      this.timeout(1000000000)
+      it('should a list of all foods with their id, name and calories', (done) => {
+        var ourRequest = this.request
+        ourRequest.get('/api/v1/foods', function(error, response) {
+          if (error) { done(error) }
+          Food.findAll()
+            .then(function(data){
+              var id = data.rows[0].id
+              var name =  data.rows[0].name
+              var calories =  data.rows[0].calories
+              eval(pry.it)
+              ourRequest.get('/api/v1/foods', function(error, response){
+                if (error) { done(error) }
+                var parsedFood = JSON.parse(response.body)
+                assert.equal(parsedFood[0].id, id)
+                assert.equal(parsedFood[0].name, name)
+                assert.equal(parsedFood[0].calories, calories)
+                assert.ok(parsedFood[0].createdAt)
+                done()
+              })
+            })
+        })
+      })
+    })
   })
-  
+
 })
