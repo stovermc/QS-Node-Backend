aa27211eb3d7f82bdb03ac7b85b285b2f76c44f5
stovermc <stovermc@gmail.com>
2017-07-01 18:30:10 -0500

    Add PUT to foods endpoint
    * added PUT endpoint for foods
    
    *added updateFood function
    
    * added test for PUT endpoint
    
    * added sad path tests
    
    *api calls are now returning an array of objects

--- server.js
+++ server.js
@@ -41,11 +41,21 @@ app.get('/api/v1/foods/:id', function(request, response){
   const id = request.params.id
   Food.findFood(id)
     .then(function(data) {
-      const foodData = data
 
       if (data.rowCount == 0) { return response.sendStatus(404) }
       const rawFood = data.rows
-      response.json({ rawFood })
+      response.json(rawFood)
+    })
+})
+
+app.put('/api/v1/foods/:id', function(request, response){
+  const id = request.params.id
+  const params = request.body
+  Food.updateFood(params, id)
+    .then(function(data){
+      let food = data
+      if(!food) { response.sendStatus(404) }
+      response.json(data)
     })
 })
 
