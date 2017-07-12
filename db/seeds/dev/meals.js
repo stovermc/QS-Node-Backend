ac9051baabc53adee88b7e2e8a6849b388fc5cf5
Mark Stover <stovermc@gmail.com>
2017-07-12 15:58:25 -0600

    Revert "Chain creation of meals in seeds"

--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -1,25 +1,23 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  return knex.raw('TRUNCATE meals RESTART IDENTITY')
   .then(function() {
-      return knex.raw(
+    return Promise.all([
+      knex.raw(
         'INSERT INTO meals (name, caloricGoal, createdAt) VALUES (?, ?, ?)',
         ['Breakfast', 400, new Date]
-      )
-    }).then(function() {
-      return knex.raw(
+      ),
+      knex.raw(
         'INSERT INTO meals (name, caloricGoal, createdAt) VALUES (?, ?, ?)',
         ['Lunch', 600, new Date]
-      )
-    }).then(function() {
-      return knex.raw(
+      ),
+      knex.raw(
         'INSERT INTO meals (name, caloricGoal, createdAt) VALUES (?, ?, ?)',
-        ['Dinner', 800, new Date]
-      )
-    }).then(function() {
-      return knex.raw(
+        ['Dinner', 600, new Date]
+      ),
+      knex.raw(
         'INSERT INTO meals (name, caloricGoal, createdAt) VALUES (?, ?, ?)',
         ['Snacks', 200, new Date]
-      )
-    })
+      ),
+    ]);
   });
 };
